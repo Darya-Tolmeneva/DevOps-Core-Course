eeed1e2f0008012913e2a0806de96abf03d0aa2b
Dinosaurrik <78600651+Darya-Tolmeneva@users.noreply.github.com>
2026-04-23 21:56:45 +0300

    fix tests

--- app_python/app.py
+++ app_python/app.py
@@ -53,7 +53,7 @@ DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
 START_TIME = datetime.now(timezone.utc)
 
-DATA_DIR = os.getenv("DATA_DIR", "/data")
+DATA_DIR = os.getenv("DATA_DIR", os.path.join(os.getcwd(), "data"))
 VISITS_FILE = os.path.join(DATA_DIR, "visits")
 visits_lock = Lock()
 
